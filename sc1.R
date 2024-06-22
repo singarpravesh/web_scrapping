d11757747a2aa67f65a2c9800d0fac271a51def5
Pravesh Tamang <singarpravesh@gmail.com>
2024-06-22 15:01:33 +0530

    hdjj

--- sc1.R
+++ sc1.R
@@ -28,13 +28,26 @@ remDr$navigate("https://www.99acres.com/property-in-kolkata-ffid-page1")
 urls <- remDr$findElements(using = "xpath", "//*[@class='ellipsis']") |> 
   sapply(function(x){x$getElementAttribute("href")}[[1]])
 
+price <- c()
+coordinates <- c()
+
 for (i in 1:2){
+  rD <- rsDriver(browser="firefox",chromever = NULL, port=netstat::free_port(), verbose=F)
+  remDr <- rD[["client"]]
   remDr$navigate(urls[i])
+  
+  
   price <- remDr$findElements(using = "xpath", "//*[@class='list_header_semiBold configurationCards__configurationCardsHeading']") |> 
     sapply(function(x){x$getElementText()[[1]]})
   
+  coordinates <- remDr$findElements(using = "xpath", "/html/head/script[16]") |> 
+    sapply(function(x){x$getElementText()[[1]]})
+
+  remDr$closeWindow()
   }
 
+
+remDr$closeall()
 rD <- rsDriver(browser="firefox",chromever = NULL, port = netstat::free_port() , verbose=F)
 remDr <- rD[["client"]]
 remDr$navigate(titles[1])
