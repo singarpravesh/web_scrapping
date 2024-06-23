7a385876a117101cc4f7c3a8ec0d238e0c5c5e02
Pravesh Tamang <singarpravesh@gmail.com>
2024-06-23 20:08:03 +0530

    dcdd

---
+++ sc2.R
@@ -0,0 +1,36 @@
+library(RSelenium)
+library(dplyr)
+library(rvest)
+library(jsonlite)
+
+urls
+
+# latitude
+
+
+latitude <- c()
+
+for (i in 1:2){
+  rD <- rsDriver(browser="firefox",chromever = NULL, port=netstat::free_port(), verbose=F)
+  remDr <- rD[["client"]]
+  remDr$navigate(urls[i])
+
+  html <- remDr$getPageSource()[[1]]
+  json_ld_script <- read_html(html) |> 
+  html_nodes('script[type="application/ld+json"]')
+  json_ld_data <- lapply(json_ld_script, function(x) {
+  json_str <- html_text(x)
+  fromJSON(json_str)
+  })
+  
+  latitude[i] <- json_ld_data[[3]]$geo$latitude
+
+  remDr$closeWindow()
+
+  remove(html)
+  remove(json_ld_data)
+  remove(json_ld_script)
+  remove(rD)
+  remove(remDr)
+}
+
