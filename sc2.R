ddec13f51abcedc9f6d2514bcffb640619b791ac
Pravesh Tamang <singarpravesh@gmail.com>
2024-06-25 11:21:48 +0530

    cddgd

--- sc2.R
+++ sc2.R
@@ -8,28 +8,31 @@ urls
 # latitude
 
 
-Facilities <- c()
+Top_facilities <- list()
+Other_facilities <- list()
 
 for (i in 1:2){
   rD <- rsDriver(browser="firefox",chromever = NULL, port=netstat::free_port(), verbose=F)
   remDr <- rD[["client"]]
-  remDr$navigate(urls[1])
-
-  html <- remDr$getPageSource()[[1]]
-  json_ld_script <- read_html(html) |> 
-  html_nodes('script[type="application/ld+json"]')
-  json_ld_data <- lapply(json_ld_script, function(x) {
-  json_str <- html_text(x)
-  fromJSON(json_str)
-  })
+  remDr$navigate(urls[i])
   
-  latitude[i] <- json_ld_data[[3]]$geo$latitude
-  longitude[i] <- json_ld_data[[3]]$geo$longitude
+  html_page_main <- remDr$getPageSource()[[1]]
+  
+  Top_facilities[i] <- read_html(html_page_main) |> 
+    html_nodes('div[class="UniquesFacilities__xidFacilitiesCard"]') %>% 
+    html_text()
+  
+  remDr$findElement(using = "css", value = ".UniquesFacilities__pageHeadingWrapper > a:nth-child(2)")$clickElement()
+                       
+  html_page <- remDr$getPageSource()[[1]]
+  Other_facilities[i] <- read_html(html_page) |> 
+    html_nodes('div[class="body_med"]') %>% 
+    html_text()
+  
+  
   remDr$closeWindow()
-
-  remove(html)
-  remove(json_ld_data)
-  remove(json_ld_script)
+  remove(html_page_main)
+  remove(html_page)
   remove(rD)
   remove(remDr)
 }
