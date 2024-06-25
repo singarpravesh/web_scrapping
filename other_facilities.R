dcf048d127464c4d78becd5e28ae1916482190ce
Pravesh Tamang <singarpravesh@gmail.com>
2024-06-25 13:08:36 +0530

    cdcd

--- other_facilities.R
+++ other_facilities.R
@@ -16,10 +16,14 @@ for (i in 1:2){
   remDr$navigate(urls[1])
   
   remDr$findElement(using = "css", value = ".ReraDisclaimer__topDisclaimer > div:nth-child(1) > div:nth-child(2) > button:nth-child(1)")$clickElement()
+  remDr$buttondown()
+  
+  remDr$executeScript("window.scrollTo(0,document.body.scrollHeight);")
+  
   remDr$findElement(using = "id", value = "#selectedTab")$clickElement()
   
   remDr$findElement(using = "css", value = ".UniquesFacilities__pageHeadingWrapper > a:nth-child(2)")$clickElement()
-  
+  remDr$findElement(using = "css", value = ".UniquesFacilities__pageHeadingWrapper > a:nth-child(2)")$clickElement()
   html_page <- remDr$getPageSource()[[1]]
   Other_facilities[i] <- read_html(html_page) |> 
     html_nodes('div[class="body_med"]') %>% 
