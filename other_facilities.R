5acae2c61bdb569ee17757819525103c64751468
Pravesh Tamang <singarpravesh@gmail.com>
2024-06-25 11:41:05 +0530

    vffvfv

---
+++ other_facilities.R
@@ -0,0 +1,34 @@
+library(RSelenium)
+library(dplyr)
+library(rvest)
+library(jsonlite)
+
+urls
+
+# latitude
+
+
+Other_facilities <- list()
+
+for (i in 1:2){
+  rD <- rsDriver(browser="firefox",chromever = NULL, port=netstat::free_port(), verbose=F)
+  remDr <- rD[["client"]]
+  remDr$navigate(urls[i])
+  
+  remDr$findElement(using = "css", value = ".ReraDisclaimer__topDisclaimer > div:nth-child(1) > div:nth-child(2) > button:nth-child(1)")$clickElement()
+  
+  remDr$findElement(using = "css", value = ".UniquesFacilities__pageHeadingWrapper > a:nth-child(2)")$clickElement()
+  
+  html_page <- remDr$getPageSource()[[1]]
+  Other_facilities[i] <- read_html(html_page) |> 
+    html_nodes('div[class="body_med"]') %>% 
+    html_text() %>% list()
+  
+  
+  remDr$closeWindow()
+  
+
+  remove(html_page)
+  remove(rD)
+  remove(remDr)
+}
--- sc2.R
+++ sc2.R
@@ -9,7 +9,7 @@ urls
 
 
 Top_facilities <- list()
-Other_facilities <- list()
+
 
 for (i in 1:2){
   rD <- rsDriver(browser="firefox",chromever = NULL, port=netstat::free_port(), verbose=F)
@@ -20,21 +20,13 @@ for (i in 1:2){
   
   Top_facilities[i] <- read_html(html_page_main) |> 
     html_nodes('div[class="UniquesFacilities__xidFacilitiesCard"]') %>% 
-    html_text()
+    html_text() %>% list()
   
-  remDr$findElement(using = "css", value = ".ReraDisclaimer__topDisclaimer > div:nth-child(1) > div:nth-child(2) > button:nth-child(1)")$clickElement() %>% 
-    remDr$findElement(using = "css", value = ".UniquesFacilities__pageHeadingWrapper > a:nth-child(2)")$clickElement()
-                       
-  html_page <- remDr$getPageSource()[[1]]
-  Other_facilities[i] <- read_html(html_page) |> 
-    html_nodes('div[class="body_med"]') %>% 
-    html_text()
-  
+ 
   
   remDr$closeWindow()
   
   remove(html_page_main)
-  remove(html_page)
   remove(rD)
   remove(remDr)
 }
