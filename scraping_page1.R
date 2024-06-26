6e768699b3bfc3d1923612289bcd9c06d5f3ddb0
Pravesh Tamang <singarpravesh@gmail.com>
2024-06-26 12:39:28 +0530

    bv

--- scraping_page1.R
+++ scraping_page1.R
@@ -41,31 +41,31 @@ Distance_to_locational_advantage <- list()
 
 # Scrape the data in page 1
 
-rD <- rsDriver(browser="firefox",chromever = NULL, port=netstat::free_port(), verbose=F)
-remDr <- rD[["client"]]
-remDr$navigate(urls[1])
-# Click the ok button
-remDr$findElement(using = "css", value = ".ReraDisclaimer__topDisclaimer > div:nth-child(1) > div:nth-child(2) > button:nth-child(1)")$clickElement()
 
 tic("Time")
 
 
-for (i in 0:3){
+for (i in 1:length(urls)){
   
   # Navigate
-
+  rD <- rsDriver(browser="firefox",chromever = NULL, port=netstat::free_port(), verbose=F)
+  remDr <- rD[["client"]]
+  remDr$navigate(urls[1])
+  
+  # Click the ok button
+  remDr$findElement(using = "css", value = ".ReraDisclaimer__topDisclaimer > div:nth-child(1) > div:nth-child(2) > button:nth-child(1)")$clickElement()
   
    
   # Data for price
-  Price[i+1] <- remDr$findElements(using = "xpath", "//*[@class='list_header_semiBold configurationCards__configurationCardsHeading']") |> 
+  Price[i] <- remDr$findElements(using = "xpath", "//*[@class='list_header_semiBold configurationCards__configurationCardsHeading']") |> 
     sapply(function(x){x$getElementText()[[1]]})
   
   # BHK data
-  Bhk[i+1] <- remDr$findElements(using = "xpath", "//*[@class='ellipsis list_header_semiBold configurationCards__configurationCardsSubHeading']") |> 
+  Bhk[i] <- remDr$findElements(using = "xpath", "//*[@class='ellipsis list_header_semiBold configurationCards__configurationCardsSubHeading']") |> 
     sapply(function(x){x$getElementText()[[1]]})
  
   # Area data
-  Area_sqft[i+1] <- remDr$findElements(using = "xpath", "//*[@class='caption_subdued_medium configurationCards__cardAreaSubHeadingOne']") |> 
+  Area_sqft[i] <- remDr$findElements(using = "xpath", "//*[@class='caption_subdued_medium configurationCards__cardAreaSubHeadingOne']") |> 
     sapply(function(x){x$getElementText()[[1]]}) %>% 
     list()
   
@@ -77,11 +77,11 @@ for (i in 0:3){
     json_str <- html_text(x)
     fromJSON(json_str)
   })
-  Latitude[i+1] <- json_ld_data[[3]]$geo$latitude
-  Longitude[i+1] <- json_ld_data[[3]]$geo$longitude
+  Latitude[i] <- json_ld_data[[3]]$geo$latitude
+  Longitude[i] <- json_ld_data[[3]]$geo$longitude
  
   # Top facilities
-  Top_facilities[i+1] <- read_html(html) |> 
+  Top_facilities[i] <- read_html(html) |> 
     html_nodes('div[class="UniquesFacilities__xidFacilitiesCard"]') %>% 
     html_text() %>% list()
   
@@ -90,7 +90,7 @@ for (i in 0:3){
   remDr$setTimeout(type = "implicit", milliseconds = 10000) # Need to wait to load the page in the remote driver
   remDr$findElement(using = "css", value = ".UniquesFacilities__pageHeadingWrapper > a:nth-child(2)")$clickElement() # Click on the View all button
   html_page <- remDr$getPageSource()[[1]] # get the html content of the pop up page after click
-  Other_facilities[i+1] <- read_html(html_page) |> 
+  Other_facilities[i] <- read_html(html_page) |> 
     html_nodes('div[class="body_med"]') %>% 
     html_text() %>% list()
   
@@ -99,23 +99,24 @@ for (i in 0:3){
   remDr$setTimeout(type = "implicit", milliseconds = 10000)
   remDr$findElement(using = "css", value = ".OrderComponent__leftSection > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(2)")$clickElement()
   html_page <- remDr$getPageSource()[[1]]
-  Locational_advantages[i+1] <- read_html(html_page) |> 
+  Locational_advantages[i] <- read_html(html_page) |> 
     html_nodes('div[class="list_header_semiBold spacer2 ellipsis"]') %>% 
     html_text() %>% list()
   
   # Distance to locational advantages as specified above. 
-  Distance_to_locational_advantage[i+1] <- read_html(html_page) |> 
+  Distance_to_locational_advantage[i] <- read_html(html_page) |> 
     html_nodes('div[class="caption_subdued_medium ellipsis"]') %>% 
     html_text() %>% list()
   
-  remDr$executeScript(sprintf("window.open('%s');", urls[i+2]))
-  Sys.sleep(10)
+  remDr$closeWindow()
   
   # Remove the objects that can clutter the environment
   remove(html)
   remove(json_ld_data)
   remove(json_ld_script)
   remove(html_page)
+  remove(rD)
+  remove(remDr)
   }
 
 toc()
