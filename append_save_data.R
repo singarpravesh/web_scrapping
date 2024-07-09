8014dc802573a53c0fa578231429158ddbb2cc3c
Pravesh Tamang <singarpravesh@gmail.com>
2024-07-09 18:24:18 +0530

    cdscs

--- append_save_data.R
+++ append_save_data.R
@@ -2,7 +2,7 @@ library(tidyverse)
 housing_data1 <- mget(paste0('housing_data_page', 1:25)) |> 
   bind_rows()
 
-housing_data2 <- mget(paste0('housing_data_page', 26:28)) |> 
+housing_data2 <- mget(paste0('housing_data_page', 26:31)) |> 
   bind_rows() |> 
   unnest_wider(c(1, 2),names_sep = "_") |> 
   select(c(1,3,5:11)) |> 
@@ -12,9 +12,8 @@ housing_data2 <- mget(paste0('housing_data_page', 26:28)) |>
          bhk = bhk_1)
 
 housing_data <-  bind_rows(housing_data1, housing_data2)  
-housing_data |>   
-unnest_wider(c(1:3, 6:9),names_sep = "_") |> sapply(class)
-  write.csv("housing_data.csv")
 
-read_csv("housing_data.csv") -> csv
+saveRDS(housing_data, "housing_data.rds")
+
+
 
